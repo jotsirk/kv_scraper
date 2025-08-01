7863fbe7234217ce10e3b094a099613ddeabf14c
Kristo Jürgenson <kristo.jyrgenson.ext@cybexer.com>
2025-08-01 16:00:08 +0300

    added mui template dashboard

--- scraper-fe/src/index.js
+++ scraper-fe/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Dashboard from "./dashboard/Dashboard";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#1976d2' },
+    secondary: { main: '#dc004e' },
+  },
+  typography: {
+    fontFamily: '"Roboto", sans-serif',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Dashboard></Dashboard>
+    {/*
+    */}
   </React.StrictMode>
 );
 
